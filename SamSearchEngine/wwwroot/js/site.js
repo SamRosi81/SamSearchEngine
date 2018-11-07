05071c846f869c94c832d30a5c0ffa52a22fa22d
saros0300 <sam.roshan@ucsit.se>
2018-11-07 11:30:35 +0100

    Initial commit to import the project

---
+++ SamSearchEngine/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+(function () {
+    var cx = '004623051103029271626:bvfbk3gamxc'; // Custom Search engine ID here
+    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+    gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+})();
